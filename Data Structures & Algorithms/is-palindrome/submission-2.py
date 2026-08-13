acee7c8f99a23e95c15388ce8683f4c8359dc82d
wandering-node <36694505+wandering-node@users.noreply.github.com>
2026-08-13 12:23:58 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pure_s = ''
+        # extract the alphanumerical characters in the string
+        for char in s:
+            if char.isalnum():
+                pure_s += char
+            else:
+                continue
+        if pure_s:
+            for i in range(len(pure_s)//2 + 1):
+                if pure_s[i].lower() != pure_s[-i-1].lower():
+                    return False
+        return True
+
