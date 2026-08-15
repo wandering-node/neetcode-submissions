142b856f5a8cda11ecd483bbf1422e01002d34aa
wandering-node <36694505+wandering-node@users.noreply.github.com>
2026-08-15 18:55:29 +0800

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        tracker = [0] * 26
+        for char in s1:
+            tracker[ord(char)-ord('a')] += 1
+
+        for i in range(len(s2)-len(s1)+1):
+            tmp_str = s2[i:i+len(s1)]
+            tmp_tracker = [0] * 26
+            for char in tmp_str:
+                tmp_tracker[ord(char)-ord('a')] += 1
+            if tmp_tracker == tracker:
+                return True
+        return False
