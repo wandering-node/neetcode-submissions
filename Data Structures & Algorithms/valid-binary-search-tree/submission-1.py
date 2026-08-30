3f29c0effaa4ec2abe6e377b9e4098d4c5a5ee2d
wandering-node <36694505+wandering-node@users.noreply.github.com>
2026-08-30 16:38:43 +0800

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        lower = float('-inf')
+        upper = float('inf')
+        if not root:
+            return True
+        def dfs(node, lower, upper):
+            if not node:
+                return True
+            if lower < node.val < upper:
+                return dfs(node.left, lower, node.val) and dfs(node.right, node.val, upper)
+            return False
+        return dfs(root, lower, upper)
