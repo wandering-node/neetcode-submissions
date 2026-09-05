e1540c2a416dedde3555ac971db6884bf332100a
wandering-node <36694505+wandering-node@users.noreply.github.com>
2026-09-05 17:29:22 +0800

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        edge_map = collections.defaultdict(list)
+        for u,v in edges:
+            edge_map[u].append(v)
+            edge_map[v].append(u)
+        part = 0
+        visited = set()
+        for i in range(n):
+            if i not in visited:
+                part += 1
+                queue = collections.deque([i])
+                visited.add(i)
+                while queue:
+                    curr = queue.popleft()
+                    for nei in edge_map[curr]:
+                        if nei not in visited:
+                            visited.add(nei)
+                            queue.append(nei)
+        return part
