9dfa3e88efdfbc4a864246c02efb67773adfb26e
wandering-node <36694505+wandering-node@users.noreply.github.com>
2026-09-14 15:21:42 +0800

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        clone_map = {}
+        def clone(node):
+            if not node:
+                return
+            if node in clone_map:
+                return clone_map[node]
+            new_node = Node(node.val)
+            clone_map[node] = new_node
+            for nei in node.neighbors:
+                new_node.neighbors.append(clone(nei))
+            return new_node
+        return clone(node)
+            
